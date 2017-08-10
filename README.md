149468345ffe73eea69fb022f3b5a463897cb22b
Jan Curn <jan.curn@gmail.com>
2017-08-10 12:29:20 +0200

    Development of first version

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # act-probe-page-resources
 
 Apify act that sequentially loads a list of URLs in headless Chrome
-and extracts a list of resources from each of them.
+and analyzes HTTP resources requested by each page.
 
 The act does not store its state, i.e. if it crashes it restarts fetching all URLs.
 Therefore you should only use it for short lists of URLs.
+
+The implementation uses Chrome Debug Protocol rather than Selenium. See the following resources
+for more details:
+
+* https://chromedevtools.github.io/devtools-protocol/
+* https://github.com/cyrus-and/chrome-remote-interface/wiki
+* https://developers.google.com/web/updates/2017/04/headless-chrome
+
 
 **INPUT**
 
@@ -15,8 +23,8 @@ Input is a JSON object with the following properties:
     // Array of URLs to open by the browser.
     urls: [String],
 
-    // Indicates how to long should the browser stay on each page before loading the next URL
-    timeoutSecs: Number,
+    // Indicates how to long should the browser wait for pending resources after page has been loaded
+    waitSecs: Number,
 }
 ```
 
--- main.js
+++ main.js
@@ -1,63 +1,120 @@
 const chromeLauncher = require('chrome-launcher');
 const CDP = require('chrome-remote-interface');
+const _ = require('underscore');
+const Apify = require('apify');
+const typeCheck = require('type-check').typeCheck;
 
 
-const launchChrome = (headless=false) => {
-    // Code inspired by https://developers.google.com/web/updates/2017/04/headless-chrome
-    return chromeLauncher.launch({
+// Definition of the input
+const INPUT_TYPE = `{
+    urls: [String],
+    waitSecs: Maybe Number
+}`;
+
+
+Apify.main(async () => {
+    // Fetch and check the input
+    const input = await Apify.getValue('INPUT');
+    if (!typeCheck(INPUT_TYPE, input)) {
+        console.log('Expected input:');
+        console.log(INPUT_TYPE);
+        console.log('Received input:');
+        console.dir(input);
+        throw new Error('Received invalid input');
+    }
+
+    // Launch Chrome
+    const chrome = await launchChrome({ headless: !!process.APIFY_HEADLESS });
+    const client = await CDP();
+
+    let currentResult = null;
+
+    // Extract domains
+    const { Network, Page } = client;
+
+    // Setup event handlers
+    Network.requestWillBeSent((params) => {
+        //console.log("### Network.requestWillBeSent");
+        //console.dir(params);
+
+        let req = currentResult.requests[params.requestId];
+        if (!req) {
+            req = currentResult.requests[params.requestId] = {};
+            req.url = params.request.url;
+            req.method = params.request.method;
+            req.requestedAt = new Date(params.wallTime * 1000);
+        } else {
+            // On redirects, the Network.requestWillBeSent() is fired multiple times
+            // with the same requestId and the subsequent requests contain the 'redirectResponse' field
+            req.redirects = req.redirects || [];
+            const redirect = _.pick(params.redirectResponse, 'url', 'status');
+            redirect.location = params.redirectResponse && params.redirectResponse.headers ? params.redirectResponse.headers['location'] : null;
+            req.redirects.push(redirect);
+        }
+    });
+
+    Network.responseReceived((params) => {
+        //console.log("### Network.responseReceived");
+        //console.dir(params);
+
+        const req = currentResult.requests[params.requestId];
+        req.loadedUrl = params.response.url;
+        req.status = params.response.status;
+        req.mimeType = params.response.mimeType;
+        req.type = params.type;
+        req.loadedAt = new Date(params.wallTime * 1000);
+    });
+
+    Network.loadingFailed((params) => {
+        //console.log("### Network.loadingFailed");
+        //console.dir(params);
+
+        // Note that request failures might come from the previous page
+        const req = currentResult.requests[params.requestId];
+        if (req) {
+            req.type = params.type;
+            req.errorText = params.errorText;
+            req.canceled = params.canceled;
+        }
+    });
+
+    // Enable events
+    await Promise.all([Network.enable(), Page.enable()]);
+
+    // Disable cache
+    await Network.setCacheDisabled({ cacheDisabled: true });
+
+    // Iterate and probe all URLs
+    const results = [];
+    for (let url of input.urls) {
+        currentResult = {
+            url,
+            requests: {}
+        };
+        results.push(currentResult);
+
+        await Page.navigate({ url });
+        await Page.loadEventFired();
+        // Wait input.waitSecs seconds
+        await new Promise((resolve) => setTimeout(resolve, input.waitSecs*1000 || 0));
+        await Page.stopLoading();
+    }
+
+    // Save results
+    await Apify.setValue('OUTPUT', results);
+
+    // Only useful for local development
+    await chrome.kill();
+});
+
+
+// Code inspired by https://developers.google.com/web/updates/2017/04/headless-chrome
+const launchChrome = async (options = {}) => {
+    return await chromeLauncher.launch({
         port: 9222,
         chromeFlags: [
-            '--window-size=412,732',
-            '--disable-gpu',
-            headless ? '--headless' : ''
+            options.headless ? '--disable-gpu' : '',
+            options.headless ? '--headless' : ''
         ]
     });
 };
-
-
-launchChrome().then((chrome) => {
-    return new Promise((resolve, reject) => {
-        CDP((client) => {
-            // extract domains
-            const { Network, Page } = client;
-            // setup handlers
-            Network.requestWillBeSent((params) => {
-                console.log("### Network.requestWillBeSent");
-                console.dir(params);
-                // console.log(params.request.url);
-            });
-            Network.responseReceived((params) => {
-                console.log("### Network.responseReceived");
-                console.dir(params);
-                //console.log(params.requestId);
-                //console.log(params.type);
-            });
-            Network.loadingFailed((params) => {
-                console.log("### Network.loadingFailed");
-                console.dir(params);
-            });
-            Page.loadEventFired(() => {
-                client.close();
-            });
-            // enable events then start!
-            Promise.all([
-                Network.enable(),
-                Page.enable(),
-            ]).then(() => {
-                return Page.navigate({ url: 'https://github.com' });
-            }).catch((err) => {
-                console.error(err);
-                client.close();
-                resolve();
-            });
-        }).on('error', (err) => {
-            // cannot connect to the remote endpoint
-            console.error(err);
-            reject(err);
-        });
-    });
-})
-.catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
